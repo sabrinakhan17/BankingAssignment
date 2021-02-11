645063cd91199bf01c82c607915b38f48cd43183
sabrinakhan17 <33761617+sabrinakhan17@users.noreply.github.com>
2021-02-11 13:54:38 -0500

    Update SavingsAccount.java
    changed amount to savingsBalance

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,28 +1,28 @@
 public class SavingsAccount extends Account{
-	private int amount;
+	private int savingBalance;
 	private int fee = 50;
 	
 	public SavingsAccount() {
 		
 	}
 	
-	public SavingsAccount(int amount, int fee) {
-		this.amount = amount;
+	public SavingsAccount(int savingBalance, int fee) {
+		this.savingBalance = savingBalance;
 		this.fee = fee;
 	}
 	
-	public SavingsAccount(int id, String name, int amount) {
+	public SavingsAccount(int id, String name, int savingBalance) {
 		setId(id);
 		setName(name);
-		this.amount = amount;
+		this.savingBalance = savingBalance;
 	}
 
-	public int getAmount() {
-		return amount;
+	public int getsavingBalance() {
+		return savingBalance;
 	}
 
-	public void setAmount(int amount) {
-		this.amount = amount;
+	public void setsavingBalance(int savingBalance) {
+		this.savingBalance = savingBalance;
 		fee();
 	}
 
@@ -35,29 +35,29 @@ public class SavingsAccount extends Account{
 	}
 	
 	public void depositSavings(int d) {
-		int newAmount = amount+d;
-		setAmount(newAmount);
+		int newAmount = savingBalance+d;
+		setsavingBalance(newAmount);
 	}
 	
 	public void withdrawlSavings(int w) {
-		if(w<amount) {
-			int newAmount = amount-w;
-			setAmount(newAmount);
+		if(w<savingBalance) {
+			int newAmount = savingBalance-w;
+			setsavingBalance(newAmount);
 		}else {
 			System.out.println("ERROR: Withdrawl Amount is Over Account Balance");
 		}
 	}
 	
 	public void fee() {
-		if(amount < 2000) {
-			this.amount = amount - fee;
+		if(savingBalance < 2000) {
+			this.savingBalance = savingBalance - fee;
 		}else {
-			this.amount = amount;
+			this.savingBalance = savingBalance;
 		}
 	}
 	
 	public void printSavingsAccountDetails() {
-		System.out.println("\t"+getId()+"\t"+getName()+"\t"+getAmount());
+		System.out.println("\t"+getId()+"\t"+getName()+"\t"+getsavingBalance());
 	}
 
 }

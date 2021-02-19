9d44dc746886df0a0846f49f9e2e3e021e9fa754
Sabrina <33761617+sabrinakhan17@users.noreply.github.com>
2021-02-19 16:57:20 -0500

    Update TestBanking.java

--- TestBanking.java
+++ TestBanking.java
@@ -1,19 +1,27 @@
-//Group 4
+//Group 4 - Spring 2021
 //Sabrina Khan, Dilpreet Kaur, Jasmilan Singh, Christopher Cruz, Tasha Gosine
-public class TestBanking {
-	
+
+/* 
+ * Test 1: Create account
+ * Test 2: Deposit
+ * Test 3: Withdrawal
+ * Test 4: Withdrawal Error
+ * Test 5: Charge Fee
+ */
+
+public class TestBanking__S2021_SchmeelkBank {
+
 	public static void main(String[] args) {
 		
+		//Intro To Bank Application
 		System.out.println("Welcome to Schmeelk's Savings Bank!\n");
 		
+		//Checking Account
 		System.out.println("----------------------------------------------------------------------------------");
 		System.out.println("Test 1: Create Checking Account");
-		CheckingAccount c1 = new CheckingAccount();
-		c1.setId(1);
-		c1.setName("Checking 1");
-		c1.setcheckingBalance(1000);
+		Checking_S2021_SchmeelkBank c1 = new Checking_S2021_SchmeelkBank(1, "Checking 1", 1000);
 		
-		System.out.println("Checking Account:\n"+"Account Number\t"+"Account Name\t"+"Amount");
+		System.out.println("Checking Account:\n" + "Account Number\t" + "Account Name\t" + "Amount");
 		c1.printCheckingAccountDetails();
 		
 		System.out.println("\nTest 2: Depositing $50");
@@ -27,16 +35,17 @@ public class TestBanking {
 		System.out.println("\nTest 4: Print Error Message if Withdrawal Balance is Less Than Account Balance");
 		c1.withdrawlChecking(2000);
 		c1.printCheckingAccountDetails();
+		System.out.println();
+	
 		
+		//Savings Account 1 - below $2000(min amount for savings account)
 		System.out.println("----------------------------------------------------------------------------------");
 		System.out.println("Test 1: Create Savings Account");
-		SavingsAccount s1 = new SavingsAccount();
-		s1.setId(1);
-		s1.setName("Savings 1");
-		s1.setsavingBalance(1000);
-		System.out.println("Test 5: Charge Fee if Balance is Less Than $2000, amount deposited is $"+s1.getsavingBalance());
+		Saving_S2021_SchmeelkBank s1 = new Saving_S2021_SchmeelkBank(1, "Savings 1", 1000);
+	
+		System.out.println("Test 5: Charge Fee if Balance is Less Than $2000, amount deposited is $" + s1.getsavingBalance());
 		s1.fee();
-		System.out.println("\nSavings Account:\n"+"Account Number\t"+"Account Name\t"+"Amount");
+		System.out.println("\nSavings Account:\n" + "Account Number\t" + "Account Name\t" + "Amount");
 		s1.printSavingsAccountDetails();
 		
 		System.out.println("\nTest 2: Depositing $1000");
@@ -50,15 +59,16 @@ public class TestBanking {
 		System.out.println("\nTest 3: Withdrawal $50");
 		s1.withdrawlSavings(50);
 		s1.printSavingsAccountDetails();
+		System.out.println();
+
 		
+		//Savings Account 2 - with over $2000(min amount for savings account)
 		System.out.println("----------------------------------------------------------------------------------");
 		System.out.println("Test 1: Create Savings Account");
-		SavingsAccount s2 = new SavingsAccount();
-		s2.setId(1);
-		s2.setName("Savings 1");
-		s2.setsavingBalance(3000);
-		System.out.println("Test 5: Charge Fee if Balance is Less Than $2000, amount deposited is $"+s2.getsavingBalance());
-		System.out.println("\nSavings Account:\n"+"Account Number\t"+"Account Name\t"+"Amount");
+		Saving_S2021_SchmeelkBank s2 = new Saving_S2021_SchmeelkBank(2, "Savings 2", 3000);
+		
+		System.out.println("Test 5: Charge Fee if Balance is Less Than $2000, amount deposited is $" + s2.getsavingBalance());
+		System.out.println("\nSavings Account:\n" + "Account Number\t" + "Account Name\t" + "Amount");
 		s2.printSavingsAccountDetails();
 		
 		System.out.println("\nTest 2: Depositing $1000");
@@ -72,13 +82,5 @@ public class TestBanking {
 		System.out.println("\nTest 4: Print Error Message if Withdrawal Balance is Less Than Account Balance");
 		s2.withdrawlSavings(10000);
 		s2.printSavingsAccountDetails();
-
 	}
 }
-/*
- * Test 1: Create account
- * Test 2: Deposit
- * Test 3: Withdrawal
- * Test 4: Withdrawal Error
- * Test 5: Charge Fee
- */

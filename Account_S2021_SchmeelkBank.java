17c0e2eb19f45e8e3e33ae52eca14e0c35ca82fe
Sabrina <33761617+sabrinakhan17@users.noreply.github.com>
2021-02-19 16:54:48 -0500

    Update and rename Account.java to Account_S2021_SchmeelkBank.java
    changed class name

--- Account_S2021_SchmeelkBank.java
+++ Account_S2021_SchmeelkBank.java
@@ -1,13 +1,16 @@
-public class Account {
-	
+//Group 4 - Spring 2021
+//Sabrina Khan, Dilpreet Kaur, Jasmilan Singh, Christopher Cruz, Tasha Gosine
+
+public class Account_S2021_SchmeelkBank {
+
 	private int id;
 	private String name;
 	
-	public Account() {
+	public Account_S2021_SchmeelkBank() {
 		
 	}
 	
-	public Account(int id, String name) {
+	public Account_S2021_SchmeelkBank(int id, String name) {
 		this.id = id;
 		this.name = name;
 	}
@@ -26,6 +29,5 @@ public class Account {
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
+	}	
 }
